4959b00333cef20ea62004f144435e0076c048f5
Nick <ntomsho4545@gmail.com>
2019-04-15 09:33:57 -0400

    Implement translucent death modal

--- src/game.js
+++ src/game.js
@@ -17,7 +17,7 @@ class Game {
 
     drawInkGauge() {
         this.ctx.beginPath();
-        this.ctx.rect(772, 568, 32, -(this.inkGauge));
+        this.ctx.rect(772, 532, 32, -(this.inkGauge));
         this.ctx.fillStyle = "black";
         this.ctx.fill();
         this.ctx.closePath();
@@ -30,6 +30,14 @@ class Game {
     }
 
     drawDeathText() {
+        this.ctx.beginPath();
+        this.ctx.globalAlpha = 0.2;
+        this.ctx.rect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.fillStyle ="gray";
+        this.ctx.fill();
+        this.ctx.globalAlpha = 1;
+        this.ctx.closePath();
+        
         this.ctx.globalCompositeOperation = 'destination-over';
         this.ctx.font = "50px Zapfino";
         this.ctx.fillStyle = "black";
